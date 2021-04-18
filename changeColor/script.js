6af15cb6aaa41f785316a03f0bb93c0cfe205b3c
itishprasad30 <itishprasad20@gmail.com>
2021-04-18 08:02:44 +0530

    chage in js

--- changeColor/script.js
+++ changeColor/script.js
@@ -1,12 +1,24 @@
-const colorBtn =document.querySelector('.colorbtn');
-const bodyBcg=document.querySelector('body');
+const colorBtn = document.querySelector(".colorbtn");
+const bodyBcg = document.querySelector("body");
 
-const colors=['yellow','red','green','#3b5998','blue','silver','pink','purple','darkSlateGray'];
+const colors = [
+  "yellow",
+  "red",
+  "green",
+  "#3b5998",
+  "blue",
+  "silver",
+  "pink",
+  "purple",
+  "darkSlateGray",
+];
 
-colorBtn.addEventListener('click',changeColor);
+colorBtn.addEventListener("click", changeColor);
 
-function changeColor(){
-    bodyBcg.style.backgroundColor='green';
-    let random=Math.floor(Math.random()*colors.length)
-    bodyBcg.style.backgroundColor=colors[random];
+function changeColor() {
+  bodyBcg.style.backgroundColor = "green";
+  let random = Math.floor(Math.random() * colors.length);
+  bodyBcg.style.backgroundColor = colors[random];
 }
+
+// javascript
